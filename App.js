c311993ddae4f301632db7005955d2d404688be6
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2022-12-26 16:35:12 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import React, { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+  function Inc(){
+    setCount(count + 1)
+  }
+  function Dec(){
+    setCount(count - 1)
+  }
+  function Reset(){
+    setCount(0)
+  }
+  return (
+    <div className="App">
+     <h1>Counter App</h1>
+     <h2>{count}</h2>
+     <button className="btn" onClick={Inc}>Increment</button>
+     <button className="btn" onClick={Dec}>Decrement</button>
+     <button className="btn" onClick={Reset}>Reset</button>
+    </div>
+     );
+     }
+     export default App;
+
+
+     
